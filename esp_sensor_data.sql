1f8cf71afbaf959cddfed525801855b916a164ba
Esha Sanghavi <76554359+EshaSanghavi@users.noreply.github.com>
2022-09-10 20:08:30 +0530

    Add files via upload

---
+++ esp_sensor_data.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Sep 10, 2022 at 08:07 PM
+-- Server version: 8.0.30-0ubuntu0.20.04.2
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `esp_sensor_data`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `esp_sensor_data`
+--
+
+CREATE TABLE `esp_sensor_data` (
+  `reading_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `temperature_data` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `esp_sensor_data`
+--
+ALTER TABLE `esp_sensor_data`
+  ADD PRIMARY KEY (`reading_time`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
